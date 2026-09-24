854d753a7eea52b691aa2a7f396fbdc74a816f46
Malthe Rud <maltherud@gmail.com>
2026-09-24 10:17:36 +0200

    forkin

--- container.sh
+++ container.sh
@@ -3,3 +3,7 @@ docker run -d --name web nginx:alpine
 docker run -d --name dock1 nginx:alpine
 docker exec web ls -la /usr/share/nginx/html
 docker logs web
+docker stop web
+docker stop dock1
+docker rm web
+docker rm dock1
