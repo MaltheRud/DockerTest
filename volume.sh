60089b4061e9e8fc941db03564e5e4d4dc06ba6e
Malthe Rud <maltherud@gmail.com>
2026-09-24 11:36:15 +0200

    forkin

--- volume.sh
+++ volume.sh
@@ -4,3 +4,5 @@ docker volume create data
 docker run --rm -v data:/data alpine sh -c "echo hej > /data/besked.txt"
 docker run --rm -v data:/data alpine cat /data/besked.txt
 docker volume rm data
+
+! docker run--rmalpine cat /data/besked.txt
