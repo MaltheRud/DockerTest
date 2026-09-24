9b17b5b4a24666f603e83d46869446d3b0e90306
Malthe Rud <maltherud@gmail.com>
2026-09-24 11:28:09 +0200

    forkin

--- volume.sh
+++ volume.sh
@@ -1,6 +1,6 @@
 set -e
 
-
+docker volume create data
 docker run --rm -v data:/data alpine sh -c "echo hej > /data/besked.txt"
-docker run --rm -v min-volume:/data alpine cat /data/besked.txt
-docker volume rm min-volume
+docker run --rm -v data:/data alpine cat /data/besked.txt
+docker volume rm data
